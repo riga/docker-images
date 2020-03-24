0ca44fc26e14786e1ff2686e9012cc021147b672
Marcel R <marcelrieger@me.com>
2020-03-24 12:53:00 +0100

    Restructure directories, add ml images.

--- build.sh
+++ build.sh
@@ -1,35 +1,48 @@
 #!/usr/bin/env bash
 
-build_py() {
+build_image() {
+    local this_file="$( [ ! -z "$ZSH_VERSION" ] && echo "${(%):-%x}" || echo "${BASH_SOURCE[0]}" )"
+    local this_dir="$( cd "$( dirname "$this_file" )" && pwd )"
+
     local image="$1"
     local version="$2"
-    local nopush="$3"
+    local user="${3:-riga}"
 
-    if [ "$image" != "base" ] && [ "$image" != "sci" ]; then
+    local context_dir="$this_dir/$image"
+    local docker_file="Dockerfile_$( echo "$version" | sed "s/\.//g" )"
+    local tag="$user/$image:$version"
+
+    if [ ! -d "$context_dir" ]; then
         2>&1 echo "unknown image '$image'"
         return "1"
     fi
 
-    if [ "$version" = "2" ]; then
-        docker build -t riga/py-$image:2.7 py-$image-27
-        docker tag riga/py-$image:2.7 riga/py-$image:2
-        if [ "$nopush" != "1" ]; then
-            docker push riga/py-$image:2.7
-            docker push riga/py-$image:2
-        fi
-    elif [ "$version" = "3" ]; then
-        docker build -t riga/py-$image:3.7 py-$image-37
-        docker tag riga/py-$image:3.7 riga/py-$image:3
-        docker tag riga/py-$image:3.7 riga/py-$image:latest
-        if [ "$nopush" != "1" ]; then
-            docker push riga/py-$image:3.7
-            docker push riga/py-$image:3
-            docker push riga/py-$image:latest
-        fi
-    else
-        2&>1 echo "unknown version '$version'"
-        return "1"
+    if [ ! -f "$context_dir/$docker_file" ]; then
+        2>&1 echo "unknown version '$version' for image '$image'"
+        return "2"
     fi
-}
 
-export -f build_py
+    (
+        echo "building image $tag from $context_dir/$docker_file"
+        cd "$context_dir"
+        docker build --file "$docker_file" --tag "$tag" .
+    )
+}
+[ -z "$ZSH_VERSION" ] && export -f build_image
+
+build_all() {
+    local user="${1:-riga}"
+    local nopush="${2:-0}"
+
+    # python images
+    for version in 2.7 3.7 3.8; do
+        for image in py-base py-sci py-ml; do
+            build_image "$image" "$version" "$user"
+
+            if [ "$nopush" != "1" ]; then
+                docker push "$user/$image:$version"
+            fi
+        done
+    done
+}
+[ -z "$ZSH_VERSION" ] && export -f build_all
